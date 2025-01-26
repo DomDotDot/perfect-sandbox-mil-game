cb244befa274176d27be7d8ce547336c7c0afa6c
DomDotDot <112331168+DomDotDot@users.noreply.github.com>
2025-01-26 04:23:53 +0300

    Revert "Deploy website - based on 796c15c240f77c85f4c543deb3d8fe861650d9f4"

--- assets/js/36994c47.73afddba.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkperfect_sandbox_mil_game=self.webpackChunkperfect_sandbox_mil_game||[]).push([[9858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
--- assets/js/5e95c892.7406e4fe.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkperfect_sandbox_mil_game=self.webpackChunkperfect_sandbox_mil_game||[]).push([[9647],{8122:(e,s,r)=>{r.r(s),r.d(s,{default:()=>d});r(6540);var a=r(4164),c=r(1082),n=r(204),t=r(2831),u=r(1410),l=r(4848);function d(e){return(0,l.jsx)(c.e3,{className:(0,a.A)(n.G.wrapper.docsPages),children:(0,l.jsx)(u.A,{children:(0,t.v)(e.route.routes)})})}}}]);
